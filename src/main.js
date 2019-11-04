d5fb1dbadef5e28b72d66ef6cf87236512ac2008
MaricruzJ <maricruzj.er@gmail.com>
2019-11-04 10:44:03 -0500

    Actualización de archivos

--- src/main.js
+++ src/main.js
@@ -1,28 +1,2 @@
-import POKEMON from './data/pokemon/pokemon.js';
-import { orderData } from './data.js';
-
-const list = document.getElementById("list");
-
-const order = orderData(POKEMON);
-
-order.forEach(element => {
-
-const divGeneral = document.createElement('div');
-
-const divName = document.createElement('p');
-divName.innerHTML = element.name;
-
-const divImage = document.createElement('img');
-divImage.setAttribute("src",element.img);
-
-divGeneral.appendChild(divImage);
-divGeneral.appendChild(divName);
-
-list.appendChild(divGeneral);
-
-});
-
-
-
 
 
