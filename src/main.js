3521ff2575abd0d9431494595f1c6b6386522b08
analobaton <lobaton.001@gmail.com>
2019-11-05 13:11:18 -0500

    cambios main js

--- src/main.js
+++ src/main.js
@@ -14,20 +14,24 @@ order.forEach(data => {
   const divGeneral = document.createElement('div');
   divGeneral.classList.add('item-pokemon');
 
-
   document.getElementById("datos").style.display = 'none';
 
   divGeneral.onclick = (event) => {
     document.getElementById("datos").style.display = 'block';
     document.getElementById("root").style.display = 'none';
-    if (data.next_evolution !== undefined) {
-      data.next_evolution.forEach(x => {
-        const nextEvolution = x.name;
-        console.log(nextEvolution);
-      });
-      console.log(nextEvolution);
-      
-    }
+    
+    let datosEvolucion="";
+     if (data.next_evolution !== undefined) {
+       datosEvolucion= data.next_evolution
+ datosEvolucion.forEach (object => {
+ 
+  console.log(datosEvolucion.values(object));
+  console.log(datosEvolucion.values(object));
+ 
+ })
+ 
+ }
+    };
 
     document.getElementById("datos").innerHTML =
     `<div>
@@ -40,10 +44,11 @@ order.forEach(data => {
     <p>Talla: ${data.height}</p>
     <p>Multiplicadores: ${data.multiplier}</p>
     <p>Debilidades: ${data.weaknesses}</p>
-  </div > `
-  
-  }
+    <p>Evolución:
 
+    <P></P>
+    </p>
+  </div > `
   
 
   const p = document.createElement('p');
@@ -57,29 +62,3 @@ order.forEach(data => {
 
   list.appendChild(divGeneral);
 });
-
-
-/* order.forEach(data => {
-  const divGeneral = document.createElement('div');
-  divGeneral.classList.replace('item-pokemon', 'showDatos');
-
-  divGeneral.onclick = () => { }
-
-  const p = document.createElement('p');
-  const divImage = document.createElement('img');
-
-  divImage.setAttribute("src", data.img);
-  p.innerHTML = data.name;
-  p.innerHTML = data.type;
-  p.innerHTML = data.height;
-  p.innerHTML = data.weight;
-  p.innerHTML = data.candy;
-  p.innerHTML = data.multipliers;
-  p.innerHTML = data.weaknesses;
-  p.innerHTML = data.next_evolution;
-
-  divGeneral.appendChild(divImage);
-  divGeneral.appendChild(p);
-
-  datos.appendChild(divGeneral);
-}); */
