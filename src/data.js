35cfb6f908411a70d1d3e689ad74562cbd9ff69c
analobaton <lobaton.001@gmail.com>
2019-11-06 13:08:49 -0500

    cambios data y main

--- src/data.js
+++ src/data.js
@@ -1,7 +1,11 @@
 
 export const orderData = (dataPokemon) => {
-  let resultOrder = dataPokemon.sort((a,b)=>{
-    return (a.name<b.name) ? -1 : 1
+  let resultOrder = dataPokemon.sort((a, b) => {
+    return (a.name < b.name) ? -1 : 1
   });
   return resultOrder
+};
+
+export const search = (dataBase, searchBy) => {
+ return dataBase.find(data => data.num === searchBy);
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import POKEMON from './data/pokemon/pokemon.js';
 import { orderData } from './data.js';
+import { search } from './data.js';
 
 const list = document.getElementById("list");
 const order = orderData(POKEMON);
@@ -19,22 +20,9 @@ order.forEach(data => {
   divGeneral.onclick = (event) => {
     document.getElementById("datos").style.display = 'block';
     document.getElementById("root").style.display = 'none';
-    
-    let datosEvolucion="";
-     if (data.next_evolution !== undefined) {
-       datosEvolucion= data.next_evolution
- datosEvolucion.forEach (object => {
- 
-  console.log(datosEvolucion.values(object));
-  console.log(datosEvolucion.values(object));
- 
- })
- 
- }
-    };
 
     document.getElementById("datos").innerHTML =
-    `<div>
+      `<div>
     <img src="${data.img}", alt="${data.name}">
     <p>Nombre: ${data.name}</p> 
     <p>Tipo: ${data.type} </p>
@@ -44,12 +32,35 @@ order.forEach(data => {
     <p>Talla: ${data.height}</p>
     <p>Multiplicadores: ${data.multiplier}</p>
     <p>Debilidades: ${data.weaknesses}</p>
-    <p>Evolución:
-
-    <P></P>
-    </p>
+    <div id="evolucion">Evolución:
+    </div>
   </div > `
-  
+
+    // console.log(data.next_evolution);  
+    let evolucion = document.getElementById('evolucion');
+
+    data.next_evolution.forEach(element => {
+
+      let image = document.createElement('img');
+      let name = document.createElement('p');
+
+      name.innerHTML = element.name;
+
+      let x = search(order, element.num);
+      console.log(x);
+
+      image.setAttribute("src", x.img)
+
+      
+
+
+      evolucion.appendChild(image);
+      evolucion.appendChild(name);
+
+
+    });
+
+  };
 
   const p = document.createElement('p');
   const divImage = document.createElement('img');
