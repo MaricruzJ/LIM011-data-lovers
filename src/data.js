9d2e2087cef404f10d190a010eebb29af075dd5c
Ana Lobaton <lobaton.001@gmail.com>
2019-11-24 17:38:52 -0500

    cambios css

--- src/data.js
+++ src/data.js
@@ -1,24 +1,100 @@
+export const orderData = (dataPokemon, orderBy) => {
+  let resultOrder = [];
 
-export const orderData = (dataPokemon) => {
-  const resultOrder = dataPokemon.sort((a, b) => {
-    let abc = 0;
-    if (a.name < b.name) abc = -1;
-    if (a.name > b.name) abc = 1;
-    return abc;
-  });
+  if (orderBy === '1') {
+    resultOrder = dataPokemon.sort((a, b) => {
+      let abc = 0;
+      if (a.weaknesses.length < b.weaknesses.length) abc = -1;
+      if (a.weaknesses.length > b.weaknesses.length) abc = 1;
+      return abc;
+    });
+  } else if (orderBy === '2') {
+    resultOrder = dataPokemon.sort((a, b) => {
+      let abc = 0;
+      if (a.weaknesses.length > b.weaknesses.length) abc = -1;
+      if (a.weaknesses.length < b.weaknesses.length) abc = 1;
+      return abc;
+    });
+  } else {
+    resultOrder = dataPokemon.sort((a, b) => {
+      let abc = 0;
+      if (a.name < b.name) abc = -1;
+      if (a.name > b.name) abc = 1;
+      return abc;
+    });
+  }
   return resultOrder;
 };
 
-export const searchData = (dataBase, searchBy) => {
-  console.log(searchBy);
-
+export const searchPokemon = (dataBase, searchBy) => {
   const a = dataBase.find((data) => data.num === searchBy);
   return a;
 };
 
 export const search = (dataBase, searchBy) => {
-  console.log(searchBy);
-
-  const a = dataBase.find((data) => data.name.toUpperCase() === searchBy);
-  return a;
+  let x = [];
+  dataBase.forEach((element) => {
+    if (element.name.toUpperCase().indexOf(searchBy) > -1) {
+      x.push(element);
+    }
+  });
+  return x;
 };
+
+export const topDiez = (dataPokemon) => {
+
+  dataPokemon.sort((a, b) => ((a.spawn_chance < b.spawn_chance) ? 1 : -1));
+  const resultTopDiez = dataPokemon.slice(0, 10);
+  return resultTopDiez;
+};
+
+export const filters = (data, filter, value) => {
+  return data.filter((poke) => {
+    const pokeValue = poke[filter];
+    if (typeof pokeValue === 'object') return pokeValue.indexOf(value) > -1;
+    return pokeValue;
+  });
+};
+
+export const getOption = (POKEMON, property) => {
+  const elements = [];
+  POKEMON.forEach((pokemon) => {
+    pokemon[property].forEach((value) => {
+      if (elements.indexOf(value) > -1) return;
+      elements.push(value);
+    });
+  });
+  return elements;
+};
+
+export const candies = (data, rango) => {
+  if (rango === '1') {
+    return data.filter((element) => {
+      if (element.candy_count > 0 && element.candy_count < 26) {
+        return element;
+      }
+    });
+  }
+  if (rango === '2') {
+    return data.filter((element) => {
+      if (element.candy_count > 25 && element.candy_count < 51) {
+        return element;
+      }
+    });
+  }
+  if (rango === '3') {
+    return data.filter((element) => {
+      if (element.candy_count > 50 && element.candy_count < 101) {
+        return element;
+      }
+    });
+  }
+  if (rango === '4') {
+    return data.filter((element) => {
+      if (element.candy_count > 100) {
+        return element;
+      }
+    });
+  }
+
+}
--- src/main.js
+++ src/main.js
@@ -1,109 +1,220 @@
 import POKEMON from './data/pokemon/pokemon.js';
-import { orderData, searchData, search } from './data.js';
+import { orderData, searchPokemon, search, filters, getOption, topDiez, candies } from './data.js';
 
 const list = document.getElementById('list');
+const profile = document.getElementById('profile');
 const btnBack = document.getElementById('btn-back');
+const menu = document.getElementById('menu');
+const filtros = document.getElementById('filtros');
+const home = document.getElementById('home');
+
+menu.addEventListener('click', () => {
+  if (filtros.classList.contains('hidden')) {
+    filtros.classList.remove('hidden');
+    filtros.classList.add('show');
+  } else {
+    filtros.classList.remove('show');
+    filtros.classList.add('hidden');
+  }
+});
 
 btnBack.addEventListener('click', () => {
-  document.getElementById('profile').style.display = 'none';
-  document.getElementById('home').style.display = 'flex';
-  document.getElementById('header').style.display = 'block';
-  document.getElementById('home').style.marginTop = '0px';
-  document.getElementById('home').style.padding = '16px';
-  document.getElementById('root').style.paddingTop = '128px';
+  document.getElementById('content-profile').style.display = 'none';
+  home.style.display = 'flex';
+  home.style.marginTop = '0px';
+  home.style.padding = '16px';
 });
 
-const order = orderData(POKEMON);
-
-document.getElementById('search').addEventListener('click', () => {
-  const textSarch = document.getElementById('search').value;
-  let abc = search(order, textSarch.toUpperCase());
-  console.log(abc);
-});
-
-order.forEach((data) => {
-  const divGeneral = document.createElement('div');
-  divGeneral.classList.add('item-pokemon');
-
-  const p = document.createElement('p');
-  const divImage = document.createElement('img');
-
-  p.innerHTML = data.name;
-  divImage.setAttribute('src', data.img);
-
-  divGeneral.appendChild(divImage);
-  divGeneral.appendChild(p);
-
-  list.appendChild(divGeneral);
-
-  document.getElementById('profile').style.display = 'none';
-
-  divGeneral.addEventListener('click', () => {
-    document.getElementById('header').style.display = 'none';
-    document.getElementById('root').style.paddingTop = '0px';
-    document.getElementById('profile').style.display = 'block';
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('header').style.display = 'none';
-
-    let template = `<div class = 'item-pokemon'>
-      <img src='${data.img}', alt='${data.name}'>
-      <p>${data.name}</p> 
-      </div>
-      <div class = 'detail-pokemon'>
-      <p>Tipo: ${data.type} </p>
-      <p>Candy: ${data.candy}</p>
-      <p>Candy-Count: ${data.candy_count}</p>
-      <p>Peso: ${data.weight}</p>
-      <p>Talla: ${data.height}</p>
-      <p id ='multipliers'>Multiplicadores:</p>
-      <p>Debilidades: ${data.weaknesses}</p>
-      </div>
-      <div id='evolution'>
-        <div id='preEvolution'>
-        </div>
-        <div id='nextEvolution'>
-        </div>
-      </div> `;
-
-    document.getElementById('profile').innerHTML = template;
-
-    const nextEvolution = document.getElementById('nextEvolution');
-    const preEvolution = document.getElementById('preEvolution');
-
-    const templateEvolution = (hi) => {
-      const title = document.createElement('p');
-      hi.innerHTML = 'Siguiente Evolución';
-
-      hi.appendChild(title);
-
-      data.next_evolution.forEach((next) => {
-        const div = document.createElement('div');
-        div.setAttribute('class', 'item-pokemon');
-        const image = document.createElement('img');
-        const name = document.createElement('p');
-
-        div.appendChild(image);
-        div.appendChild(name);
-
-        name.innerHTML = next.name;
-
-        const x = searchData(order, next.num);
-
-        image.setAttribute('src', x.img);
-
-        div.setAttribute('id', x.id);
-        hi.appendChild(div);
-      });
-    };
-
-    if (data.next_evolution !== undefined) {
-      templateEvolution(nextEvolution);
-    }
-
-    if (data.prev_evolution !== undefined) {
-      templateEvolution(preEvolution);
-    }
+// datos ordenados de forma ascendente.
+const sortData = orderData(POKEMON);
 
 
+const templateDetail = (pokemon) => {
+  const templateTwo = `
+                <div class='content-detail'>
+                <div class='left'>
+                  <p> Pokemon Seleccionado</p>
+                  <div class='item-pokemon'>
+                    <img src='${pokemon.img}' , alt='${pokemon.name}'>
+                    <p>${pokemon.name}</p>
+                  </div>
+                </div>
+                <div class='center'>
+                  <div class='detail-pokemon'>
+                    <p>Tipo: ${pokemon.type} </p>
+                    <p>Candy: ${pokemon.candy}</p>
+                    <p>Candy-Count: ${pokemon.candy_count}</p>
+                    <p>Peso: ${pokemon.weight}</p>
+                    <p>Talla: ${pokemon.height}</p>
+                    <p id='multipliers'>Multiplicadores:</p>
+                    <p>Debilidades: ${pokemon.weaknesses}</p>
+                  </div>
+                </div>
+                <div class='rigth'>
+                  <div id='evolution'>
+                    <div id='preEvolution' class=''>
+                    </div>
+                    <div id='nextEvolution' class=''>
+                    </div>
+                  </div>
+                </div>
+              </div>`;
+  profile.innerHTML = templateTwo;
+};
+
+const general = (xy) => {
+  console.log('funcion', xy);
+
+  xy.forEach((data) => {
+
+    const divGeneral = document.createElement('div');
+    divGeneral.classList.add('item-pokemon');
+
+    const p = document.createElement('p');
+    const divImage = document.createElement('img');
+
+    p.innerHTML = data.name;
+    divImage.setAttribute('src', data.img);
+
+    divGeneral.appendChild(divImage);
+    divGeneral.appendChild(p);
+
+    list.appendChild(divGeneral);
+
+    document.getElementById('content-profile').style.display = 'none';
+
+    divGeneral.addEventListener('click', () => {
+      document.getElementById('content-profile').style.display = 'block';
+      filtros.classList.remove('show');
+      filtros.classList.add('hidden');
+      home.style.display = 'none';
+
+      templateDetail(data);
+
+      const nextEvolution = document.getElementById('nextEvolution');
+      const preEvolution = document.getElementById('preEvolution');
+
+      if (data.next_evolution !== undefined) {
+        const title = document.createElement('p');
+        title.innerHTML = 'Next Evolución';
+
+        nextEvolution.appendChild(title);
+
+        data.next_evolution.forEach((next) => {
+          const divEvolution = document.createElement('div');
+          divEvolution.setAttribute('class', 'item-pokemon');
+          const image = document.createElement('img');
+          const name = document.createElement('p');
+
+          divEvolution.appendChild(image);
+          divEvolution.appendChild(name);
+
+          name.innerHTML = next.name;
+
+          const y = searchPokemon(sortData, next.num);
+          image.setAttribute('src', y.img);
+
+          nextEvolution.appendChild(divEvolution);
+        });
+      }
+
+      if (data.prev_evolution !== undefined) {
+        const title = document.createElement('p');
+        title.innerHTML = 'Pre Evolución';
+
+        preEvolution.appendChild(title);
+
+        data.prev_evolution.forEach((next) => {
+          const divEvolution = document.createElement('div');
+          divEvolution.setAttribute('class', 'item-pokemon');
+          const image = document.createElement('img');
+          const name = document.createElement('p');
+
+          divEvolution.appendChild(image);
+          divEvolution.appendChild(name);
+
+          name.innerHTML = next.name;
+
+          const y = searchPokemon(x, next.num);
+          image.setAttribute('src', y.img);
+
+          preEvolution.appendChild(divEvolution);
+        });
+      }
+    });
   });
+};
+
+general(sortData);
+
+const topTen = document.getElementById('topTen');
+
+const mostrarTopTen = topDiez(sortData);
+
+topTen.addEventListener('click', () => {
+  list.innerHTML = '';
+  general(mostrarTopTen);
+});
+
+let showFiltersOrder = filters(POKEMON, 'filter', 'value');
+const selectTypeFilters = document.querySelector('#id_type');
+
+getOption(POKEMON, 'type').map((element) => {
+  const option = document.createElement('option');
+  option.setAttribute('value', element);
+  option.innerText = element;
+
+  selectTypeFilters.appendChild(option);
+
+  selectTypeFilters.addEventListener('change', (event) => {
+    showFiltersOrder = filters(POKEMON, 'type', event.target.value);
+    list.innerHTML = '';
+    general(showFiltersOrder);
+  });
+});
+
+const selectWeaknessesFilters = document.querySelector('#id_weaknesses');
+getOption(sortData, 'weaknesses').map((element) => {
+  const option = document.createElement('option');
+  option.setAttribute('value', element);
+  option.innerText = element;
+
+  selectWeaknessesFilters.appendChild(option);
+
+  selectWeaknessesFilters.addEventListener('change', (event) => {
+    showFiltersOrder = filters(sortData, 'weaknesses', event.target.value);
+    list.innerHTML = '';
+    general(showFiltersOrder);
+  });
+});
+
+const selectSort = document.querySelector('#selectSort');
+selectSort.addEventListener('change', () => {
+  list.innerHTML = '';
+  const sortBy = orderData(sortData, selectSort.value);
+  general(sortBy);
+});
+
+const showAll = document.getElementById('showAll');
+
+showAll.addEventListener('click', () => {
+  list.innerHTML = '';
+  general(orderData(POKEMON));
+});
+
+document.getElementById('search').addEventListener('input', () => {
+  const textSarch = document.getElementById('search').value;
+  const abc = search(sortData, textSarch.toUpperCase());
+  list.innerHTML = '';
+  general(abc);
+});
+
+const selectCandies = document.getElementById('selectCandies');
+selectCandies.addEventListener('change', () => {
+  list.innerHTML = '';
+  if (selectCandies.value === '0') {
+    general(sortData);
+  } else {
+    general(candies(sortData, selectCandies.value));
+  }
 });
