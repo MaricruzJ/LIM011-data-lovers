8cdaadd263ff15c1c56754b2a58b4ed7f0de03fa
MaricruzJ <=>
2019-11-26 12:40:47 -0500

    test 100%

--- test/data.spec.js
+++ test/data.spec.js
@@ -7,13 +7,13 @@ describe('orderData ', () => {
   test('debería ser una función', () => {
     expect(typeof orderData).toBe('function');
   });
-  test('El primer valor de orderData para un pokemon fuerte debría de ser Snorlax', () => {
+  test('Debería de retornar "Snorlax", como nombre del primer pokemon más fuerte', () => {
     expect(orderData(POKEMON, 'strong')[0].name).toEqual('Snorlax');
   });
-  test('El primer valor de orderData para un pokemon debil debría de ser Exeggcute', () => {
+  test('Debería de retornar "Exeggutor", como nombre del primer pokemon más débil', () => {
     expect(orderData(POKEMON, 'weak')[0].name).toEqual('Exeggutor');
   });
-  test('El primer valor de orderData para ninguna condición debría de ser Abra', () => {
+  test('Debería retornar "Abra", como nombre del primer pokemon ordenado ascendentemente', () => {
     expect(orderData(POKEMON)[0].name).toEqual('Abra');
   });
 });
@@ -22,7 +22,7 @@ describe('searchPokemon ', () => {
   test('debería ser una función', () => {
     expect(typeof searchPokemon).toBe('function');
   });
-  test('Debería de retornar ...', () => {
+  test('Debería retornar "Bulbasaur", para la búsqueda del pokemon número 001', () => {
     expect(searchPokemon(POKEMON, '001').name).toBe('Bulbasaur');
   });
 });
@@ -31,8 +31,8 @@ describe('searchGroup ', () => {
   test('debería ser una función', () => {
     expect(typeof searchGroup).toBe('function');
   });
-  test('Debería retornar .', () => {
-    expect(searchGroup(POKEMON, 'ABRA')[0].name).toBe('Abra');
+  test('Debería retornar "Abra", para cuando se busca ABRA', () => {
+    expect(searchGroup(POKEMON, 'AB')[0].name).toBe('Abra');
   });
 });
 
@@ -40,7 +40,7 @@ describe('topDiez ', () => {
   test('debería ser una función', () => {
     expect(typeof topDiez).toBe('function');
   });
-  test('debería retornar .', () => {
+  test('debería retornar "Pidgey", para el primer pokemon del topTen', () => {
     expect(topDiez(POKEMON)[0].name).toBe('Pidgey');
   });
 });
@@ -49,10 +49,10 @@ describe('filters ', () => {
   test('debería ser una función', () => {
     expect(typeof filters).toBe('function');
   });
-  test('debería ...', () => {
+  test('debería retornar "Paras", para el primer pokemon de tipo grass', () => {
     expect(filters(POKEMON, 'type', 'Grass')[0].name).toBe('Paras');
   });
-  test('debería ....', () => {
+  test('debería retornar "Weedle", para el primer pokemon de debilidad Fire', () => {
     expect(filters(POKEMON, 'weaknesses', 'Fire')[0].name).toBe('Weedle');
   });
 });
@@ -61,7 +61,7 @@ describe('getOption ', () => {
   test('debería ser una función', () => {
     expect(typeof getOption).toBe('function');
   });
-  test('debería ...', () => {
+  test('debería retornar "Normal", para el primer elemento del array de tipos de pokemon', () => {
     expect(getOption(POKEMON, 'type')[0]).toBe('Normal');
   });
 });
@@ -70,19 +70,19 @@ describe('candies ', () => {
   test('debería ser una función', () => {
     expect(typeof candies).toBe('function');
   });
-  test('debería ser una función.', () => {
+  test('debería retornar "Pidgey", para el primer pokemon del rango [0-25] de candies', () => {
     expect(candies(POKEMON, '1')[0].name).toBe('Pidgey');
   });
-  test('debería ser una función..', () => {
+  test('debería retornar "Zubat", para el primer pokemon del rango [26-50] de candies', () => {
     expect(candies(POKEMON, '2')[0].name).toBe('Zubat');
   });
-  test('debería ser una función...', () => {
+  test('debería retornar "Poliwhirl", para el primer pokemon del rango [51-100] de candies', () => {
     expect(candies(POKEMON, '3')[0].name).toBe('Poliwhirl');
   });
-  test('debería ser una función....', () => {
+  test('debería retornar "Magikarp", para el primer pokemon del rango [100++] de candies', () => {
     expect(candies(POKEMON, '4')[0].name).toBe('Magikarp');
   });
-  test('debería ser una función.....', () => {
+  test('debería retornar el texto "Fuera de rango", para un rango distinto a los establecidos', () => {
     expect(candies(POKEMON, '5')).toBe('Fuera de rango');
   });
 });
